c9f729f32d08cff97f48baf88bebb94408f84d9d
Gert Poppe <gert.poppe@stackandheap.com>
2013-08-02 12:56:53 +0200

    added lock

--- src/motion-reveal-plugin.js
+++ src/motion-reveal-plugin.js
@@ -1,23 +1,33 @@
 var leapMotionController = new Leap.Controller({enableGestures: true});
 var slideReady = true;
+var locked = false;
 
 Reveal.addEventListener('slidechanged', function (event) {
     slideReady = true;
 });
+
+window.onkeydown = function(event) {
+    if(event.keyCode == '71') {
+        console.log("change lock");
+        locked = !locked;
+    }
+}
 
 leapMotionController.on('connect', function () {
     console.log("Successfully connected leap motion controller.");
 });
 
 leapMotionController.on('frame', function (event) {
-    if (event.gestures.length > 0) {
-        var gesture = event.gestures[0];
-        if (gesture.state === 'stop') {
-            if (slideReady) {
-                if (gesture.direction[0] > gesture.direction[2]) {
-                    goToPrevious();
-                } else {
-                    goToNext();
+    if(!locked) {
+        if (event.gestures.length > 0) {
+            var gesture = event.gestures[0];
+            if (gesture.state === 'stop') {
+                if (slideReady) {
+                    if (gesture.direction[0] > gesture.direction[2]) {
+                        goToPrevious();
+                    } else {
+                        goToNext();
+                    }
                 }
             }
         }
